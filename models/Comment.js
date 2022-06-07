ce942a9884c8a47de8a76206c092c3177c51866d
Tori Acer <vrice360@gmail.com>
2022-06-07 02:44:47 -0500

    uploaded the pizza data

--- models/Comment.js
+++ models/Comment.js
@@ -56,6 +56,6 @@ CommentSchema.virtual('replyCount').get(function() {
   return this.replies.length;
 });
 
-const Comment = model('Comment', CommentSchema, ReplySchema );
+const Comment = model('Comment', CommentSchema);
 
 module.exports = Comment;
--- server.js
+++ server.js
@@ -18,4 +18,6 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hun
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
 
+app.use(require('./routes'));
+
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
